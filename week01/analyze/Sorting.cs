debc364be875ecece23c44112a22feb38edfaf6d
Koigor97 <turaysamuel101@yahoo.com>
2024-11-06 10:11:12 +0200

    test performance of bubble sort using large array

--- week01/analyze/Sorting.cs
+++ week01/analyze/Sorting.cs
@@ -1,8 +1,23 @@
-﻿public static class Sorting {
+﻿using System.Diagnostics;
+
+public static class Sorting {
     public static void Run() {
-        var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };
+        // generate a large array of random integers
+        var numbers = ArrayGenerator.GenerateLargeArray(10000);
+        
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
         SortArray(numbers);
-        Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
+        // Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
+
+        stopwatch.Stop();
+        /** 
+        * Print the first 10 elements to verify sorting without overwhelming the output
+        * Then test the performance of the sorting algorithm
+        */
+     
+        Console.WriteLine("First 10 elements of sorted array: {0}", string.Join(", ", numbers[..10]));
+        Console.WriteLine("Sorting took {0}ms", stopwatch.ElapsedMilliseconds);
     }
 
     private static void SortArray(int[] data) {
